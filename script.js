f2b6b1dab503c0bfdb64809f762384966c0913d6
Hashley-Da <hashleyjonsson@gmail.com>
2025-07-30 12:49:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('stocks').innerHTML = 
+      data.stocks.map(item => `
+        <p>${item.boutique} : ${item.produit} (${item.quantite})</p>
+      `).join('');
+  });
